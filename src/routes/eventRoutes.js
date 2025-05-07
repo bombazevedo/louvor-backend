0192cab312c5ff8b408881d815886a5c807cf7dc
Robson Ferreira de Azevedo <bombazevedo7@gmail.com>
2025-05-07 14:00:52 -0300

    error9

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { authenticate } = require("../middleware/auth");
+
 const {
   getEvents,
+  getEventById,
   updateEvent,
   deleteEvent
 } = require("../controllers/eventController");
 
+// Rotas protegidas por autenticação
 router.get("/", authenticate, getEvents);
+router.get("/:id", authenticate, getEventById);
 router.patch("/:id", authenticate, updateEvent);
 router.delete("/:id", authenticate, deleteEvent);
 
 module.exports = router;
-
-router.get('/:id', getEventById);
