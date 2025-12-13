3cb0f8e0ccb60dca773a3d6586e132ed33a517b0
Robson Ferreira de Azevedo <bombazevedo7@gmail.com>
2025-12-13 19:04:11 -0300

    chore(staging): quick commit

--- src/routes/historicoRoutes.js
+++ src/routes/historicoRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { listarHistorico, listarExecucoesIndividuais } = require('../controllers/historicoController');
 const { authenticate } = require('../middleware/auth');
+const orgContext = require('../middleware/orgContext');
 
 // 🔗 Histórico consolidado com filtro de datas
-router.get('/', authenticate, listarHistorico);
+router.get('/', authenticate, orgContext, listarHistorico);
 
 // 🔗 Todas execuções individuais com data
-router.get('/execucoes', authenticate, listarExecucoesIndividuais);
+router.get('/execucoes', authenticate, orgContext, listarExecucoesIndividuais);
 
 module.exports = router;
