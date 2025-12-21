88c3d39ae42a039b90bf38ae0d43abceef6bb649
Robson Ferreira de Azevedo <bombazevedo7@gmail.com>
2025-12-21 14:26:22 -0300

    chore(staging): quick commit

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,4 +1,4 @@
-const express = require("express");router.get("/birthdays", authenticate, getBirthdays);
+const express = require("express");
 const router = express.Router();
 const { authenticate } = require("../middleware/auth");
 const {
